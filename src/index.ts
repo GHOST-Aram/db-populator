5599f1951b7185ce87b0c5c78217442f4f23c78e
ghost-aramic <owinoojala@gmail.com>
2023-11-19 19:13:31 +0300

    chore: Transfer Posts population code to a new file

--- src/index.ts
+++ src/index.ts
@@ -10,43 +10,10 @@ if(connectString){
     mongoose.connect(connectString).then(result =>{
         console.log('Connected to DB')
 
-        fetchData('https://jsonplaceholder.typicode.com/posts')
+        fetchData('https://jsonplaceholder.typicode.com/coments')
         .then(async(data: any) =>{
             
-            const authors = await User.find()
-            let dataCount = 0
-            let imageCount = 0
-
-
-                for(let i = 0; i < data.length; i += 10){
-                    
-                    authors.forEach(async(author) =>{
-        
-                        if (imageCount === images.length)
-                            imageCount = 0
-        
-                        const post = new Post({
-                            post_content: formatPostText(data[dataCount].body),
-                            author: author._id,
-                            media_url: images[imageCount]
-                        })
-                        
-                        const savedPost = await post.save()
-
-                        imageCount ++
-                        dataCount ++
-                        
-                        console.log(`Post number ${dataCount+1} created with id: ${savedPost.id} `)
-                        
-                    })
-                }
-
-            
-
-            
-            
-
-
+ 
 
         }).catch(error => console.log(
             'Fetch Failed: ', error.message
---
+++ src/posts-populator.ts
@@ -0,0 +1,61 @@
+import { User } from "./User.model";
+import { Post } from "./Post.model";
+import { fetchData, formatPostText } from "./utils"
+import { images } from "./data";
+import mongoose from "mongoose";
+import 'dotenv/config'
+
+const connectString = process.env.MONGODB_URI
+if(connectString){
+    mongoose.connect(connectString).then(result =>{
+        console.log('Connected to DB')
+
+        fetchData('https://jsonplaceholder.typicode.com/posts')
+        .then(async(data: any) =>{
+            
+            const authors = await User.find()
+            let dataCount = 0
+            let imageCount = 0
+
+
+                for(let i = 0; i < data.length; i += 10){
+                    
+                    authors.forEach(async(author) =>{
+        
+                        if (imageCount === images.length)
+                            imageCount = 0
+        
+                        const post = new Post({
+                            post_content: formatPostText(data[dataCount].body),
+                            author: author._id,
+                            media_url: images[imageCount]
+                        })
+                        
+                        const savedPost = await post.save()
+
+                        imageCount ++
+                        dataCount ++
+                        
+                        console.log(`Post number ${dataCount+1} created with id: ${savedPost.id} `)
+                        
+                    })
+                }
+
+            
+
+            
+            
+
+
+
+        }).catch(error => console.log(
+            'Fetch Failed: ', error.message
+        ))
+
+    }).catch(error => console.log(
+        'Failed to connect to DB: ', error.message
+    ))
+
+} else {
+    console.log('Connection string not found')
+}
