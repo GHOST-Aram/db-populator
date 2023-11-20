1d7af1ea9cec30148422be4045bffdbe31a1fa29
ghost-aramic <owinoojala@gmail.com>
2023-11-20 13:41:06 +0300

    refactor: Change signature of createPost function

--- src/posts/posts-data.ts
+++ src/posts/posts-data.ts
@@ -4,9 +4,14 @@ import { formatText } from "../utils/formatter"
 import { images } from "../utils/data";
 import { Types } from "mongoose";
 import { dbIsConnected } from "../utils/db";
-            
+
+interface PostData{
+    text: string, 
+    authorId: Types.ObjectId, 
+    imageIndex: number
+}
 export const createPost = (
-    text: string, authorId: Types.ObjectId, imageIndex: number
+    {text, authorId,imageIndex}:PostData
 ) =>{
     return new Post({
         post_content: formatText(text),
@@ -18,36 +23,35 @@ export const createPost = (
 export const findAllPosts = async() =>{
     return await Post.find().select('id')
 }
+
 export const populatePosts = async(
     {authors, data }:{authors: HydratedUserDoc[], data:any}
 ) =>{
     let dataCount = 0
-    let imageCount = 0
 
     for(let i = 0; i < data.length; i += 10){
-        
-        authors.forEach(async(author) =>{
-
-            if (imageCount === images.length)
-                imageCount = 0
-
-            if(dataCount < data.length){
+        authors.forEach(async(author, index) =>{
+            if(dataCount >= data.length){
                 return
             }
             
             const post = createPost(
-                data[dataCount].body, author._id, imageCount
+                {
+                    text:data[dataCount].body, 
+                    authorId:author._id, 
+                    imageIndex:index
+                }
             )
+            dataCount ++
 
             console.log(`post ${dataCount+1} id: ${post.id}`)
-            imageCount ++
-            dataCount ++
             
 
             if(dbIsConnected()){
                 const savedPost = await post.save()
                 console.log(
-                    `Post number ${dataCount+1} created with id: ${savedPost.id} `)
+                    `Post number ${dataCount+1} created with id: ${savedPost.id} `
+                )
             }
         })
     }
